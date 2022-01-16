722207b9d72fcc0e4b4a1842ef764d297c6d460d
yinweb <famil91218@gmail.com>
2022-01-16 17:44:40 +0800

    20220116用jquery 控版漢堡列開起與關閉

--- javascript/main.js
+++ javascript/main.js
@@ -34,7 +34,7 @@
     function phonenav() {
         // 新增標籤 ul和 li
         let ul = document.createElement('ul');
-        let li = document.createElement('li');
+        
 
         // 在 menunav 內新增，宣告變數
         let SN = document.getElementById('phonenav');
@@ -43,7 +43,44 @@
         SN.appendChild(ul);
         // 賦予屬性 id or  class
         ul.setAttribute("id" , "phonenav");
-        let Items =['品牌故事','購物說明','客服留言','門市資訊'];
-        
+
+        let phonenav = document.getElementById('phonenav');
+
+        let navclass =['about','verticallline','shop','verticallline','meassage','verticalline','address'];
+
+        let navcon = ['品牌故事','|','購物說明','|','客服留言','|','門市資訊'];
+
+        // 新增標籤時，程式的運作方式為:先新增標籤，賦予屬性，在指定新增的外框架(此為一個迴圈)
+        // 回圈內的數字7，是因為陣列而產生，故要隨著陣列值的數量而自動增減
+
+        for(let i =0 ; i <navclass.length ; i++) {
+            let li = document.createElement('li');
+            // 新增屬性
+            li.setAttribute("class",navclass[i]);
+            // ul內新增 li 清單
+            phonenav.appendChild(li);
+
+            // 新增連結a
+            let A =document.createElement('a');
+            //  新增屬性  假連結 javascript:;
+            A.setAttribute("href","javascript:;");
+            // li 內新增 a 連結
+            li.appendChild(A);
+            // 置入 陣列內容
+            A.innertext = navcon[i];
+        }
 
     }
+
+    // 行動裝置 選單
+    menunav();
+    function menunav() {
+        let  menunav = document.getElementById('menunav');
+        // 隱藏 手機版選單
+        menunav.style.display = "none";
+    }
+
+    // pageload();
+    // function pageload() {
+    //     $('header').load("../web/header.html");
+    // }
---
+++ javascript/web.js
@@ -0,0 +1,17 @@
+// phonemenushow2();
+    function phonemenushow2() {
+        // $jQuery 的變數前綴詞
+        // 隱藏選單
+        $('#menunav').hide();
+
+        // 開啟的選單
+        $('#menubtn').click( function() {
+            $('#menunav').show();
+        });
+
+        // 關閉選單
+        $('#close').click( function() {
+            $('#menunav').hide();
+        });
+        
+    }
